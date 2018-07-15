1059968c9c37e7adc271431ab9ae80edd250518f
Андрей cb Михеев <takethis@mail.ru>
2018-07-15 17:19:20 +0300

    first push. Yahhhoooooo!

---
+++ android/src/net/overmy/dunno/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package net.overmy.dunno;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import net.overmy.dunno.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
---
+++ desktop/src/net/overmy/dunno/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.overmy.dunno.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.overmy.dunno.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
